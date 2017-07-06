9744a129030b92208fe536e05d71ee70faeea121
dthrasher90 <dthrasher90@GMAIL.COM>
2017-07-06 09:23:33 -0800

    google maps working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Weather map listening port 5000')
 });
 
